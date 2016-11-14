50f4986a2bda99483ee8c9fbece2d463349a65ba
Travis <tssund93@gmail.com>
2016-11-13 20:48:45 -0500

    insert picross into db table

--- run.lisp
+++ run.lisp
@@ -4,6 +4,7 @@
   (load "load.lisp"))
 
 (r)
+(load "env.lisp")
 
 (setf hunchentoot:*acceptor* (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 5555)))
 (setf (hunchentoot:acceptor-document-root hunchentoot:*acceptor*) "./static/")
--- sql/picross.sql
+++ sql/picross.sql
@@ -1,5 +1,7 @@
 CREATE TABLE picross (
      picross_id    SERIAL,
      picross_cells text,
+     picross_width int,
+     picross_height int,
      picross_date  timestamp
 )
