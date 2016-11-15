8012e04feff1c135d28cf44db7b66d97dbb4720e
Travis <tssund93@gmail.com>
2016-11-14 22:05:26 -0500

    started working on picross validation

--- static/picross.js
+++ static/picross.js
@@ -102,3 +102,11 @@ function toggleMode() {
 	$("#modeLink").text("switch to mark mode");
     }
 }
+
+function validatePicross(picross) {
+    $.get("/validate-picross?cells=" + makePicrossList(picross),
+	  function(data) {
+	      console.log(data);
+	      return data === "1" ? true : false;
+	  });
+}
