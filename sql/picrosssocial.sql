21071e04112c8ca38242079b36ffbce1ea134d0e
Travis <tssund93@gmail.com>
2016-11-19 10:02:15 -0500

    keep track of difficulty and display on browse page

--- sql/picrosssocial.sql
+++ sql/picrosssocial.sql
@@ -6,3 +6,9 @@ ADD picross_completable boolean;
 UPDATE picross
 SET picross_complete_count = 0,
     picross_completable = false;
+
+ALTER TABLE picross
+ADD picross_attempt_count int;
+
+UPDATE picross
+SET picross_attempt_count = 0;
