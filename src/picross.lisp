17cdcfa68a95ed339e0e43ad21f55ad927fe6c23
Travis <tssund93@gmail.com>
2016-12-28 22:17:20 -0500

    delete commented out code

--- src/picross.lisp
+++ src/picross.lisp
@@ -10,16 +10,8 @@
       (:meta :name "viewport"
              :content "width=device-width, initial-scale=1")
 
-      ;; (:link :rel "stylesheet"
-      ;;        :href "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css")
-      ;; (:link :rel "stylesheet"
-      ;;        :href "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css")
       (:link :rel "stylesheet" :href "/style.css")
 
-      ;; (:script :src "https://code.jquery.com/jquery-3.1.1.min.js"
-      ;;          :integrity "sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8 "
-      ;;          :crossorigin "anonymous")
-      ;; (:script :src "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js")
       (:script :src "/picross.js"))))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
